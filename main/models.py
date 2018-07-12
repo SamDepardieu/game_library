0b563b44734f106863a577560b9b9ef78d102eff
SamDepardieu <sam.depardieu@gmail.com>
2018-07-12 18:08:16 +0100

    :art: Add str representation to the models

--- main/models.py
+++ main/models.py
@@ -4,12 +4,21 @@ from django.db import models
 class GameType(models.Model):
     name = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 
 class Console(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
 
 
 class Game(models.Model):
     name = models.CharField(max_length=256)
     type = models.ForeignKey(GameType, on_delete=models.CASCADE)
     console = models.ForeignKey(Console, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}({self.console.name})'
